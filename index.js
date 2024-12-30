2123df8c44f65b395a00c1b6c357b56f5a82c1e3
linhdr141020 <81572858+linhdr141020@users.noreply.github.com>
2024-12-30 14:05:33 +0700

    f

--- index.js
+++ index.js
@@ -1,59 +1,17 @@
 import axios from "axios"
 import express from "express"
 import cors from "cors"
-import Redis from "ioredis";
-const client = new Redis("127.0.0.1:6379");
 const app = express()
 const port = 3004
+import expressGraphQL from "express-graphql"
+import { graphql } from "graphql"
 app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-const DEFAULT_EXPIRATION = 600;
-app.get("/git-profile", async (req, res) => {
-    const gitId = req.query.gitId;
-    const profile = await getOrSetCache(`profile?gitId=${gitId}`, async () => {
-        console.log("Cache Miss");
-        const response = await fetch(`https://api.github.com/users?gitId=${gitId}`, {
-            headers: { "Content-Type": "application/json" },
-            method: "GET",
-        });
-        const data = await response.json();
-        client.setex(`profile?gitId=${gitId}`, DEFAULT_EXPIRATION, JSON.stringify(data));
-        return data;
-    })
-    res.json(profile)
-    // client.get(`profile?gitId=${gitId}`, async (error, profile) => {
-    //     if(error) console.error(error);
-    //     if(profile != null) {
-    //         console.log("Cache Hit");
-    //         return res.json(JSON.parse(profile));
-    //     } else {
-    //         console.log("Cache Miss");
-    //         const response = await fetch(`https://api.github.com/users?gitId=${gitId}`, {
-    //             headers: { "Content-Type": "application/json" },
-    //             method: "GET",
-    //         });
-    //         const data = await response.json();
-    //         client.setex(`profile?gitId=${gitId}`, DEFAULT_EXPIRATION, JSON.stringify(data))
-    //         res.json(data)
-    //     }
-    // })
-})
-const getOrSetCache = (key, callback) => {
-    return new Promise((resolve, reject) => {
-        client.get(key, async (error, data) => {
-            if (error) return reject(error);
-            if (data != null) {
-                console.log("Cache Hit");
-                return resolve(JSON.parse(data))
-            };
-            const freshData = await callback();
-            client.setex(key, DEFAULT_EXPIRATION, JSON.stringify(freshData));
-            resolve(freshData);
-        })
-    })
-}
+app.use("/graphql", expressGraphQL({
+    graphiql: true
+}))
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
